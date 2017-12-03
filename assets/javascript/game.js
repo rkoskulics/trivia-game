485794e279bd70b7ddfc3ae1441130e1fe1c49e1
Rob <robert.koskulics@me.com>
2017-12-02 21:14:32 -0500

    Starting to get the timer working

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Some code for the timer
+var number = 10;
+
+var intervalInfo;
+
+var timerDone = "";
+
+$("#start").on("click", run);
+
+
+function run() {
+      intervalInfo = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+
+      number--;
+
+      $("#showTimer").html("<h2>" + number + "</h2>");
+
+      if (number === 0) {
+
+        stop();
+
+      }
+    }
+
+function stop() {
+
+    clearInterval(intervalInfo);
+
+    timerDone = "Timer Up!"
+
+    $("showTimer").html("<h2>" + timerDone + "</h2>");
+
+    console.log("Timer Done");
+
+
+    }
