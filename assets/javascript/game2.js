151b4f458a05fb996991b5e7951120ea5f932819
Rob <robert.koskulics@me.com>
2017-12-06 14:28:35 -0500

    Game Done! Just need to add styling

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,5 +1,5 @@
 // Some code for the timer
-var number = 10;
+var number = 5;
 
 var intervalInfo;
 
@@ -15,13 +15,26 @@ var wrongAnswers = 0;
 
 var questionAnswers = {question1: ["What is the first letter of this sentence?", "w", "h", "s", "q"],
  question2: ["When are you answering this question?", "Now", "Today", "Later", "Never"],
- question3: ["How many letters does this sentence have?", 34, 21, 2, 116]};
+ question3: ["How many letters does this sentence have?", "34", "21", "2", "116"]};
 
-
+// Start the game
 $("#start").on("click", run);
+
+// Restart the game with the initial game state
+$("#restart").on("click", restart)
+	function restart() {
+		visibleQuestion = [];
+		questionNumber = 0;
+		correctAnswers = 0;
+		wrongAnswers = 0;
+		$("#question").html("<h2></h2>");
+		$("#answers").html("<h2></h2>");
+		run();
+	}
 
 	function run() {
 		$("#start").hide();
+		$("#restart").hide();
 		$("#gameStatus").html("<h2>Game On!</h2>");
 	    intervalInfo = setInterval(decrement, 1000);	    
 	    // Create array and add the question answer combo to it
@@ -36,6 +49,7 @@ $("#start").on("click", run);
 	    	// a.text(visibleQuestion[j]);
 	    	// $("#answers").append(a);
         	var answerButton= $('<input type="button" value="' + visibleQuestion[j] + '"/>');
+        	answerButton.addClass("score");
         	answerButton.attr("data-name", visibleQuestion[j]);
         // Display the buttons
         $("#answers").append(answerButton);
@@ -55,7 +69,7 @@ $("#start").on("click", run);
 	    // Clear the window
 		$("#question").html("<h2></h2>");
 		$("#answers").html("<h2></h2>");
-		number = 10;
+		number = 5;
 		questionNumber = questionNumber + 1;
 		console.log(questionNumber);
 		// If all the questions have been used then show results
@@ -65,19 +79,28 @@ $("#start").on("click", run);
 			$("#gameStatus").html("<h2>Game Over!</h2>");
 			$("#question").html('<h2>Correct Answers: ' + correctAnswers + '</h2>');
 			$("#answers").html('<h2>Wrong Answers: ' + wrongAnswers + '</h2>');
+			$("#restart").show();
+			visibleQuestion = 0;
 		}else{
 		// Wait a few seconds then run the run function again
 		timerDone = "Get Ready For The Next Question";
 		$("#showTimer").html("<h2>" + timerDone + "</h2>");
-	    setTimeout(run, 5000);
+	    setTimeout(run, 2000);
 		}
 	}
 	// function to check if the right button is pushed
 	function scoreKeeper() {
 		var buttonValue = $(this).attr("value");
 		console.log(buttonValue);
-		stop();
+		if (buttonValue === "w" || buttonValue === "Now" || buttonValue === "34") {
+			correctAnswers = correctAnswers + 1
+			stop();
+		}else{
+			wrongAnswers = wrongAnswers + 1
+			stop();
+		}
 
 	}
+	$("#restart").hide();
 	// Create an event listener for all buttons with the ID answer
-	$(document).on("click", "#answers", scoreKeeper);
+	$(document).on("click", ".score", scoreKeeper);
