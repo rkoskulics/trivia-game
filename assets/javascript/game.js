dab7a3b6b41bc8e8e9acf183442b13b83e209d12
Rob <robert.koskulics@me.com>
2017-12-06 09:25:44 -0500

    Attempting a for loop to go through the questions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,6 @@ var intervalInfo;
 
 var timerDone = "";
 
-// Create an array for questions, then a different array of answers for each questions.
-// So if you have 4 questions, you have 4 answer arrays
-// Put the correct answer in the first place in each answer array
-//
 
 var questionAnswers = {question1: ["What is the first letter of this sentence?", "w", "h", "s", "q"],
  question2: ["When are you answering this question?", "Now", "Today", "Later", "Never"],
@@ -17,17 +13,10 @@ var questionAnswers = {question1: ["What is the first letter of this sentence?",
 
 $("#start").on("click", run);
 
-// Create an array for questions, then a different array of answers for each questions.
-// So if you have 4 questions, you have 4 answer arrays
-// Put the correct answer in the first place in each answer array
-// On click have it select a random question and display it, as well as each answer
-// Can the answers be randomized?
-
-
 	function run() {
 	    intervalInfo = setInterval(decrement, 1000);
 
-	      // Select a number at random to select an item from questionAnswers
+	    // Select a number at random to select an item from questionAnswers
 	    var visibleQuestionNumber = [Math.floor(Math.random() * 3)];
 	    console.log(visibleQuestionNumber);
 	    // Use the random number to select a random array
@@ -38,12 +27,13 @@ $("#start").on("click", run);
 	    // Make buttons based on the answers
 	    for(var i = 1; i < visibleQuestion.length; i++ ) {
           var answerButton= $('<input type="button" value="' + visibleQuestion[i] + '"/>');
+        // Display the buttons
         $("#answers").append(answerButton);
         }
 
 	      
-	      // Display the buttons
-	    }
+	    
+	}
 
 	function decrement() {
 
