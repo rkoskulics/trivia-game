4dc455bed1b13bba6c78021141b4c4dbe930fc99
Rob <robert.koskulics@me.com>
2017-12-06 11:40:13 -0500

    The questions are showing up properly

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -7,6 +7,8 @@ var timerDone = "";
 
 var visibleQuestion = [];
 
+var questionNumber = 0
+
 
 var questionAnswers = {question1: ["What is the first letter of this sentence?", "w", "h", "s", "q"],
  question2: ["When are you answering this question?", "Now", "Today", "Later", "Never"],
@@ -16,61 +18,58 @@ var questionAnswers = {question1: ["What is the first letter of this sentence?",
 $("#start").on("click", run);
 
 	function run() {
-	    intervalInfo = setInterval(decrement, 1000);
-	    // Create a loop to go through each question array
-	    for (var i = 0;i < 3; i++) {
-	    	// Create array and add the question answer combo to it
-	    	var visibleQuestion = questionAnswers[Object.keys(questionAnswers)[i]];
-	    	console.log(visibleQuestion);
-	    	// Display the question and answer on the screen
-	    	$("#question").append(visibleQuestion[0]);
-	    	// Display the answers in buttons
-	    	for(var j = 1; j < visibleQuestion.length; j++ ) {
-        		var answerButton= $('<input type="button" value="' + visibleQuestion[j] + '"/>');
-        	// Display the buttons
-        	$("#answers").append(answerButton);
-        	}
-
-	    }
-
-	    // Select a number at random to select an item from questionAnswers
-	 //    var visibleQuestionNumber = [Math.floor(Math.random() * 3)];
-	 //    console.log(visibleQuestionNumber);
-	 //    // Use the random number to select a random array
-	 //    var visibleQuestion = questionAnswers[Object.keys(questionAnswers)[visibleQuestionNumber]];
-		// console.log(visibleQuestion);
-		// // Show the question on the screen
-	 //    $("#question").append(visibleQuestion[0]);
-	 //    // Make buttons based on the answers
-	 //    for(var i = 1; i < visibleQuestion.length; i++ ) {
-  //         var answerButton= $('<input type="button" value="' + visibleQuestion[i] + '"/>');
-  //       // Display the buttons
-  //       $("#answers").append(answerButton);
-  //       }	    
+	    intervalInfo = setInterval(decrement, 1000);	    
+	    // Create array and add the question answer combo to it
+	    var visibleQuestion = questionAnswers[Object.keys(questionAnswers)[questionNumber]];
+	    console.log(visibleQuestion);
+	    // Displays the question
+		$("#question").append(visibleQuestion[0]);
+	    // Display the answers in buttons
+	    for(var j = 1; j < visibleQuestion.length; j++ ) {
+        	var answerButton= $('<input type="button" value="' + visibleQuestion[j] + '"/>');
+        // Display the buttons
+        $("#answers").append(answerButton);
+        }	    
 	}
 
+	// Display the question and answer on the screen
+	function display() {
+		// Displays the question
+		$("#question").append(visibleQuestion[0]);
+	    // Display the answers in buttons
+	    for(var j = 1; j < visibleQuestion.length; j++ ) {
+        	var answerButton= $('<input type="button" value="' + visibleQuestion[j] + '"/>');
+        // Display the buttons
+        $("#answers").append(answerButton);
+        }
+	}
+
+	
+	// Timer function
 	function decrement() {
-
 	    number--;
-
 	    $("#showTimer").html("<h2>" + number + "</h2>");
-
 	    if (number === 0) {
-
 	    stop();
-
 	    }
 	}
+
+	// function clearWindow() {
+	// 	// Clear the window
+	// 	$("#game").empty();
+	// 	number = 10;
+	// 	questionNumber = questionNumber + 1;
+	// }
 
 	function stop() {
-
 	    clearInterval(intervalInfo);
-
 	    timerDone = "Timer Up!"
-
 	    $("#showTimer").html("<h2>" + timerDone + "</h2>");
-
-	    console.log("Timer Done");
-
-
+	    // Clear the window
+		$("#question").html("<h2></h2>");
+		$("#answers").html("<h2></h2>");
+		number = 10;
+		questionNumber = questionNumber + 1;
+	    // Wait a few seconds then run the run function again
+	    setTimeout(run, 5000);
 	}
